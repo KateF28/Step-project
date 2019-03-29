f9ec56b9bb2719253afd0cffc9eb029ef3769c9f
Kate Fedorova <k_phedorova@ukr.net>
2019-03-29 02:11:03 +0200

    Style of id='work' block has been added

--- Script/script.js
+++ Script/script.js
@@ -1,12 +1,38 @@
-let chooseBtn = document.getElementsByClassName("menu__menuclause");
-console.log(chooseBtn);
+// Акордеон для блока "Our services":
+const chooseBtn = document.getElementsByClassName("service__chooseblock");
+const contentBlocks = document.getElementsByClassName("service__visibilityblock");
+
 [].forEach.call(chooseBtn, (elem) => {
-    elem.onclick = (event) => {
-        let currentBtn = [].indexOf.call(chooseBtn, event.currentTarget);
-        if (currentBtn > 1) {
-            let serviceChoose = document.getElementsByClassName("service__choose")[0];
-            serviceChoose.className = "service__chooseblock";
-            elem.className = "service__hoverchoose";
+    elem.onclick = () => {
+        if (!(elem.classList.contains('service__choosed'))) {
+            [].forEach.call(chooseBtn, (el) => {
+                el.classList.remove('service__choosed');
+            });
+            elem.classList.add('service__choosed');
+        };
+
+        let currentBtn = [].indexOf.call(chooseBtn, elem);
+        if (!(contentBlocks[currentBtn].classList.contains('.service__nonvisibilityblock'))) {
+            [].forEach.call(contentBlocks, (element) => {
+                // element.style.transform = 'translateX(50px)';
+                element.classList.remove('service__nonvisibilityblock');
+            });
+            contentBlocks[currentBtn].classList.add('service__nonvisibilityblock');
         };
     };
+});
+
+// Скрипт для блока "Our Amazing Work":
+const $workCategories = $('.work__chooseblock');
+
+// console.log($workCategories);
+[].forEach.call($workCategories, (elem) => {
+    elem.click((event) => {
+        if (!(elem.hasClass('work__choose'))) {
+            [].forEach.call($workCategories, (el) => {
+                el.removeClass('work__choose');
+            });
+            elem.addClass('work__choose');
+        };
+    });
 });
